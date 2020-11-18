6e3ba347df7aea417a510c91a645fbc85b1ca568
Peichun Luo <luopc1218@outlook.com>
2020-11-18 12:09:34 +0800

    修改eslint配置

---
+++ client/easy_video_site/.prettierrc.js
@@ -0,0 +1,15 @@
+/** @format */
+
+module.exports = {
+    printWidth: 120,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    arrowParens: 'avoid',
+    jsxSingleQuote: true,
+    // insertPragma: true,
+    tabWidth: 4,
+    useTabs: false,
+};
